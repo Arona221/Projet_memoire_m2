b3011ec1c72714224927e480d7acadd3d91c7a64
Arona Ndiaye <arona10ndiaye@gmail.com>
2024-12-18 13:32:38 +0000

    first commit

---
+++ src/main/java/connect/event/ConnectEventBackendApplication.java
@@ -0,0 +1,13 @@
+package connect.event;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectEventBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectEventBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/connect/event/ConnectEventBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package connect.event;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectEventBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
